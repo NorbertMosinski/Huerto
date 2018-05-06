6f7672de7c9bdc3902573121d5d676b05aee2181
Norbert Mosinski <norbi-95@o2.pl>
2018-05-07 00:09:30 +0200

    first structure

---
+++ js/main.js
@@ -0,0 +1,35 @@
+window.onload = function()
+{
+	init();
+}
+
+init = function()
+{
+	variables();
+	events();
+}
+
+variables = function()
+{
+
+}
+
+events = function()
+{
+	var ts = new t();
+//	doSomething();
+	ts.doSomething();
+}
+
+
+var t = function() {
+
+	 this.name = "diego";
+	this. = "rios";
+}
+
+t.Prototype.doSomething = function()
+{
+
+}
+
